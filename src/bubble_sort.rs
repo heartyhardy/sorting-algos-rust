ce0b3943d7af1de3781918cf47f4d30c5448b308
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-01-04 20:21:04 +0530

    Bubble sort

---
+++ src/bubble_sort.rs
@@ -0,0 +1,27 @@
+
+fn bubblesort(nums: &mut [i32]){
+    let (mut swp, mut l, mut i) = (false, nums.len()-1, 0);
+    while l > 0{
+        if nums[i] > nums[i+1]{
+            nums[i] ^= nums[i+1];
+            nums[i+1] ^= nums[i];
+            nums[i] ^= nums[i+1];
+            swp=true;
+        }        
+        i+=1;
+        if i == l && !swp{
+            break;
+        }else if i == l && swp{
+            i = 0;
+            l-=1;
+            swp=false;            
+        }
+    }
+}
+
+pub fn run(){
+    let mut nums: [i32;5] = [4, 5, 2, 3, 1];
+
+    bubblesort(&mut nums);
+    println!("Sorted with BubbleSort: {:?}", nums);
+}
